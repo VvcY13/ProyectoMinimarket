a49cf17fa72b3f452d670ca4fb889b892643757f
victor Caycho <vcaychoy@autonoma.edu.pe>
2023-12-03 18:44:28 -0500

    update 1.1

--- controladorEdicionProducto.php
+++ controladorEdicionProducto.php
@@ -8,6 +8,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $presentacionProducto=$_POST["txtPresentacion"];
     $categoriaProducto=$_POST["txtCategoriaProducto"];
     $cantidadProducto=$_POST["txtCantidad"];
+
+    $nuevoprecio = floatval($precioProducto);
+    $preciosinigv =$nuevoprecio-($nuevoprecio*0.18);
     
     $conexion = conectar();
     $sql = "UPDATE tblproductos 
@@ -15,7 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             Precio = '$precioProducto', 
             Descripcion = '$presentacionProducto', 
             Cantidad = '$cantidadProducto', 
-            Categoria = '$categoriaProducto' 
+            Categoria = '$categoriaProducto',
+            precioSinIGV = '$preciosinigv' 
         WHERE ID = $idProducto";
     
     if ($conexion->query($sql) === TRUE) {
